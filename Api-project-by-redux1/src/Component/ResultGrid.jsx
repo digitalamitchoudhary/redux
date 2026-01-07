ec9e019957eb14f04930cf838119b9012e52f96f
digitalamitchoudhary <amitchoudhary.dmp@gmail.com>
2026-01-07 18:05:30 +0530

    D

--- Api-project-by-redux1/src/Component/ResultGrid.jsx
+++ Api-project-by-redux1/src/Component/ResultGrid.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ResultCard from './ResultCard'
-
+import { fetchImages,fetchGIF,fetchVideos } from '../api/mediaApi'
+ 
+ 
 function ResultGrid() {
   return (
+
+          
+
     <div>
       <ResultCard/>
     </div>
